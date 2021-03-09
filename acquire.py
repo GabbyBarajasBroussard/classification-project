ab789bd2de6c5ad9dff49489bea12348274638a9
Gabby Broussard <gabriella.b.broussard@gmail.com>
2021-03-09 11:05:35 -0600

    final

--- acquire.py
+++ acquire.py
@@ -11,8 +11,6 @@ import pandas as pd
 
 
 # In[2]:
-
-
 # creating a connection to connect to the Codeup Student Database
 def get_connection(db, user=env.user, host=env.host, password=env.password):
     return f'mysql+pymysql://{user}:{password}@{host}/{db}'
